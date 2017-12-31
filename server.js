90400f23bd1d792c3d51945ef43d3892a230c8cc
Eirin <eiringonzales@gmail.com>
2017-12-31 12:57:06 +0800

    Added scripts

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.route("/")
 .get(function(req, res) {
-  res.sendFile(path.join(__dirname + '/client/public/index.html'));
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 io.on('connection', function(socket) {
